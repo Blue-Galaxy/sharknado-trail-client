0656cf28b96ea78c5ac8384450f95f52494ece19
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-01-07 13:49:29 -0700

    Private Routing capabilities and auth requests added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Base Packages
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PrivateRoute from './utilities/PrivateRoute'
 // Views
 import Game from './views/game/Game';
 import Login from './views/login/Login'
