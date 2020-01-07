2fa59c7ae60ad0c8ab66d3b10df4cf12b4076acf
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-01-07 11:07:04 -0700

    Set up basic routing for the views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+// Base Packages
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Views
+import Game from './views/game/Game';
+import Login from './views/login/Login'
+import Registration from './views/registration/Registration'
+// Styling
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Sharknado Trail</h1>
-    </div>
+    <Router>
+      {/* Public Routes */}
+      <Route exact path ="/" component={Login} />
+      <Route exact path ="/register" component={Registration} />
+      {/* Private Routes */}
+      <Route exact path ="/game" component={Game} />
+    </Router>
   );
 }
 
--- src/views/game/Game.js
+++ src/views/game/Game.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Game() {
   return (
     <div className="Game">
-      <h1>Game</h1>
+      <h1>Welcome to Sharknado Trail</h1>
     </div>
   );
 }
